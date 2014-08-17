c424f4192149aa6760858e13854eb7b016715ad3
tyxchen <tyxchen@outlook.com>
2014-08-16 23:02:35 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// Gulpfile.js
+
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify');
+
